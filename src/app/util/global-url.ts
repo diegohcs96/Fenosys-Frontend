40beb917c95d7b5cbeef65ca2bc7a8480ba3adcc
diegohcs96 <66636862+diegohcs96@users.noreply.github.com>
2021-09-23 16:30:30 -0500

    v.0.20
    - Cambios en URLs Base Backend.

--- src/app/util/global-url.ts
+++ src/app/util/global-url.ts
@@ -4,11 +4,11 @@ export const GlobalUrl = Object.freeze({
     //BASE_URL: 'http://192.168.5.144:2004/'
     
     //QA Heroku URL
-    // BASE_URL: 'https://fenosys-backend-qa.herokuapp.com/'
+    // BASE_URL: 'https://fenosys-backend.herokuapp.com/'
 
     //DEV Heroku URL
-    BASE_URL: 'https://fenosys-backend.herokuapp.com/'
-
+    BASE_URL: 'https://fenosys-backend-dev.herokuapp.com/'
+    
     //LOCALHOST URL
     //BASE_URL: 'http://localhost:8080/'
 
