f59714055ae46febd4a53ec1fdd8b03cd1c5f267
diegohcs96 <66636862+diegohcs96@users.noreply.github.com>
2021-09-28 18:45:55 -0500

    v.0.20
    Ajuste de Url Base de API's

--- src/app/util/global-url.ts
+++ src/app/util/global-url.ts
@@ -3,11 +3,11 @@ export const GlobalUrl = Object.freeze({
     //PartenerTech URL
     //BASE_URL: 'http://192.168.5.144:2004/'
     
-    //QA Heroku URL
-    // BASE_URL: 'https://fenosys-backend-qa.herokuapp.com/'
-
     //DEV Heroku URL
-    BASE_URL: 'https://fenosys-backend.herokuapp.com/'
+    BASE_URL: 'https://fenosys-backend-dev.herokuapp.com/'
+
+    //QA Heroku URL
+    //BASE_URL: 'https://fenosys-backend.herokuapp.com/'
 
     //LOCALHOST URL
     //BASE_URL: 'http://localhost:8080/'
