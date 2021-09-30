a7ee59e1e3429f6d9a4cb109c4088a8a0dc85180
diegohcs96 <66636862+diegohcs96@users.noreply.github.com>
2021-09-30 17:36:54 -0500

    v.0.25
    - Fix Base URL Backend.
    
    - Fix URL Signup Request.

--- src/app/util/global-url.ts
+++ src/app/util/global-url.ts
@@ -4,10 +4,10 @@ export const GlobalUrl = Object.freeze({
     //BASE_URL: 'http://192.168.5.144:2004/'
 
     //DEV Heroku URL
-    //BASE_URL: 'https://fenosys-backend-dev.herokuapp.com/'
+    BASE_URL: 'https://fenosys-backend-dev.herokuapp.com/'
 
     //QA Heroku URL
-    BASE_URL: 'https://fenosys-backend.herokuapp.com/'
+    //BASE_URL: 'https://fenosys-backend.herokuapp.com/'
     
     //LOCALHOST URL
     //BASE_URL: 'http://localhost:8080/'
